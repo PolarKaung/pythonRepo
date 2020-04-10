8f6cea6ba742658921a80ce54331216b8121bdb7
FrozenLad <63154242+FrozenLad@users.noreply.github.com>
2020-04-10 12:10:27 +0630

    Add files via upload

---
+++ matrix_subtraction.py
@@ -0,0 +1,16 @@
+import numpy as np
+a=np.array(((1,2,3),
+            (4,5,6),
+            (7,8,9)))
+b=np.array(((1,2,3),
+            (4,5,6),
+            (7,8,9)))
+c=np.zeros(a.shape)
+print(c)
+if a.shape==b.shape:
+    for x in range(len(a)):
+        for y in range(len(a[0])):
+            c[x,y]=a[x,y]-b[x,y]                       
+            print("The subtraction of two matrices is ",np.matrix(c))
+else:
+    print("Operation cannot be done because of unmatched size")
