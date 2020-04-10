013547ff0283dcfb1f6887161f7f66c6b6f173f7
PolarKaung <39430503+PolarKaung@users.noreply.github.com>
2020-04-10 09:07:08 +0630

    Add files via upload

---
+++ matrixAddition v2.py
@@ -0,0 +1,33 @@
+import numpy as np
+A = [
+    [1,2,3,4],
+    [2,3,4,5],
+    [5,3,6,4],
+    [3,5,3,6],
+    ]
+
+B = [
+    [1,2,3,4],
+    [2,3,4,5],
+    [5,3,6,4],
+    [3,5,3,6],
+    ]
+
+A = np.array(A)
+B = np.array(B)
+
+ASize = A.shape
+BSize = B.shape
+
+C = np.zeros(ASize)
+
+if ( ASize != BSize ):
+    print("Matrix A and B Dimension or shape must be the same!")
+else:
+    for m in range(ASize[0]):
+        for n in range(ASize[1]):
+            C[m,n] = A[m,n] + B[m,n]
+
+    print("A = ", np.matrix(A))
+    print("B = ", np.matrix(B))
+    print("C = ", np.matrix(C))
