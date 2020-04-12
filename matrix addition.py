599c47bc36001e656e18ee35a5a80a90a16026f3
Ayechanmyatphyu <63159266+Ayechanmyatphyu@users.noreply.github.com>
2020-04-12 20:18:32 +0630

    Add files via upload

---
+++ matrix addition.py
@@ -0,0 +1,17 @@
+import numpy as np
+a = np.array(((1,3),
+              (5,7)))
+b = np.array(((2,4),
+              (6,8)))
+c = np.zeros(a.shape)
+if a.shape==b.shape:
+    for m in range(len(a)):
+        for n in range(len(a[0])):
+            c[m,n] = a[m,n]+b[m,n]
+    print(c)
+else:
+    print("no result")
+
+
+
+
