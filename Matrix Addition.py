e57b01db47091a14a36320fe527f1f966037a715
kcube20 <63152165+kcube20@users.noreply.github.com>
2020-04-13 12:03:02 +0630

    Add files via upload

---
+++ Matrix Addition.py
@@ -0,0 +1,16 @@
+import numpy as np
+a = np.array([[1,2],
+              [3,4]])
+
+b = np.array([[5,6],
+              [7,8]])
+
+a_size = a.shape
+b_size = b.shape
+
+if a_size != b_size:
+    print("Can't solve. The matrices must be of the same order")
+
+else:
+    print("The Summation is")
+    print(a+b)
