ecbd9f8209c46c215b02f12b6312a2d15d6c9980
khinshunlai <63157168+khinshunlai@users.noreply.github.com>
2020-04-13 22:23:32 +0630

    Add files via upload

---
+++ matrix_transpose.py
@@ -0,0 +1,13 @@
+A = [[5,9],
+    [13 ,9],
+    [67 ,34]]
+
+R = [[0,0,0],
+    [0,0,0]]
+
+for i in range(len(A)):
+   for j in range(len([0])):
+       R[j][i] = A[i][j]
+
+for r in R:
+   print(r)
