c79adc1964410fd0d921d39a8677b4ce46e96dc9
chanmyae91 <63158948+chanmyae91@users.noreply.github.com>
2020-04-15 12:34:18 +0630

    Update salesman_salary.py

--- salesman_salary.py
+++ salesman_salary.py
@@ -1,12 +1,4 @@
-#to calculate the salary of a camera salesman.
-#basic salary is 1500,
-#for every camera he will sell
-#he will get 200 and
-#the commission on the month’s sale is 2 %.
-#The input will be number of cameras sold and total price of the cameras.
 
-
-camera_sold = int(input("Enter the number of cameras sold = "))
 camera_price = float(input("Enter the total price of cameras sold ="))
 
 basic_salary = 1500
