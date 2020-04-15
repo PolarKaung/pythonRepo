decca1b823c13a1906b2856b26eed71067824bb3
MinThant47 <63152338+MinThant47@users.noreply.github.com>
2020-04-14 22:53:12 -0700

    Update Fibonacci series v1.py

--- Fibonacci series v1.py
+++ Fibonacci series v1.py
@@ -1,8 +1,8 @@
 num = int(input("Enter the number of Fibonacci Serie: "))
 Sum = 0
-fabi = [1,1]
+fabi = [0,1]
 
-for i in range(num-2): 
+for i in range(num-1): 
     Sum = fabi[i]+fabi[i+1]
     fabi.append(Sum)
 
