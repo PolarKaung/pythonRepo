3f813a4a0ad84cca62a1a4e4b2509d376427f460
MinThant47 <63152338+MinThant47@users.noreply.github.com>
2020-04-15 20:27:28 -0700

    Update Let's print.py

--- Let's print.py
+++ Let's print.py
@@ -1,5 +1,5 @@
 a = int(input("Enter the number to expand: "))
-b = str(input("Enther whatever you like to expand: ")) # (eg. *, &, ^)
+b = str(input("Enther whatever you like to expand: ")) # (eg. *, &, ^) It'll change everything to string.
 for i in range(a):
     print("%s" %b*a)
     a = a-1
