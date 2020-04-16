0ea4ec0b3c43e1807b803a3b3b04bc7f40102e47
MinThant47 <63152338+MinThant47@users.noreply.github.com>
2020-04-15 20:26:42 -0700

    Update Let's print.py

--- Let's print.py
+++ Let's print.py
@@ -1,5 +1,5 @@
 a = int(input("Enter the number to expand: "))
-b = str(input("Enther whatever you like to expand: "))
+b = str(input("Enther whatever you like to expand: ")) # (eg. *, &, ^)
 for i in range(a):
     print("%s" %b*a)
     a = a-1
