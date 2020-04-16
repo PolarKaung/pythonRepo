a0c8fed500d2121628d194147b556c3192be6919
MinThant47 <63152338+MinThant47@users.noreply.github.com>
2020-04-16 09:50:16 +0630

    Add files via upload

---
+++ power serie.py
@@ -0,0 +1,15 @@
+coe = []
+A = []
+num = int(input("Enter the number to expand: "))
+C = float(input("Enter the constant: "))
+for i in range(num):
+    a = float(input("Enter the value of a%d: " %i))
+    coe.append(a)
+
+for j in range(num):
+    [print("%f(x-%f)^%f + "%(coe[j],C,j), end = ' ')]
+print("...")
+    
+ 
+    
+    
