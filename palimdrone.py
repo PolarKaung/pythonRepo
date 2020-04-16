f09b42cab1b21b3d792f34696ee4e5226a151e3f
MinThant47 <63152338+MinThant47@users.noreply.github.com>
2020-04-15 20:22:49 -0700

    Update palimdrone.py

--- palimdrone.py
+++ palimdrone.py
@@ -2,19 +2,25 @@ A = [input("Enter a senstence or a word to check: ")]
 output = []
 
 B = []
-B[:0] = A[0]
+B[:0] = A[0] #to change string to char in the list
 
 print("The original word: ",B)
 
+#to remove space if needed
+
 for i in range(len(B)):
     if " " in B:
-        B.remove(" ")
+        B.remove(" ") 
 print(B)
 C = len(B) 
-    
+
+#to reverse the input (space_removed)
+
 for b in range(C):
     output.append(B[-b-1])
 print("The change of order: ",output)
+
+#to check if they are the same
 
 compare = B == output
 if compare == True:
