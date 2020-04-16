7119f474c8fdc19ddd2dd847312770ebc6106870
MinThant47 <63152338+MinThant47@users.noreply.github.com>
2020-04-15 20:21:11 -0700

    Update power serie.py

--- power serie.py
+++ power serie.py
@@ -1,10 +1,15 @@
-coe = []
+coe = [] 
 A = []
 num = int(input("Enter the number to expand: "))
 C = float(input("Enter the constant: "))
+
+#To get coeffients in one list
+
 for i in range(num):
     a = float(input("Enter the value of a%d: " %i))
     coe.append(a)
+
+#print the output
 
 for j in range(num):
     [print("%f(x-%f)^%f + "%(coe[j],C,j), end = ' ')]
