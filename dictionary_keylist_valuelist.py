f89ba032e7e2fd90abe809416f4738309f55ba22
Ayechanmyatphyu <63159266+Ayechanmyatphyu@users.noreply.github.com>
2020-04-20 23:09:48 +0630

    Add files via upload

---
+++ dictionary_keylist_valuelist.py
@@ -0,0 +1,8 @@
+dict = {'a': 1, 'b':2, 'c':3}
+
+keys = dict.keys()
+keylist = list(keys)
+print("Key List =", keylist)
+values = dict.values()
+valuelist = list(values)
+print("Value List =", valuelist)
