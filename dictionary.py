ac7078bc566ecebf87ce2934358bba0205c5e5a8
FrozenLad <63154242+FrozenLad@users.noreply.github.com>
2020-04-21 07:36:16 +0630

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+dictionary={"brand": "Ford",
+            "model": "Mustang",
+            "year": 2020}
+for key, value in dictionary.items():
+    print("Keys : ",key)
+for key, value in dictionary.items():
+    print("Values : ",value)
+    
