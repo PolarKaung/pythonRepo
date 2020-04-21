6504e6f26b12580f934fba5600b8141477386fa5
Esther191 <63152155+Esther191@users.noreply.github.com>
2020-04-21 07:44:53 +0630

    Add files via upload

--- Palindrome.py
+++ Palindrome.py
@@ -1,17 +1,16 @@
 print("Testing if something is a palindrome")
 print("A word or a string is said to be palindrome if reverse of the string is same as that word or that string.")
 s = input("Please enter a word or string: ")
-s = list(s.lower())
-print(s)
-while " " in s:
-    s.remove(" ")
-while "." in s:
-    s.remove(".")
-while "'" in s:
-    s.remove("'")
-z = s[::-1]
-print()
-if s == z:
+s = s.lower()
+# converting ch in string into unicode point of that ch
+list = [ord(ch) for ch in s]
+z =[]
+#sorting out a to z
+for num in list:
+    if num >= 97 and num <= 122:
+        z.append(num)
+#palindrome checking
+if z == z[::-1]:
     print("The string is a palindrome")
 else:
     print("The string is not a palindrome")
