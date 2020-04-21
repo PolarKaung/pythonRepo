b56041d35ac66a0dc974f304ddd50fb3cebdb7cf
Esther191 <63152155+Esther191@users.noreply.github.com>
2020-04-21 18:20:38 +0630

    Add files via upload

--- Parsing Dictionary into key list and value list.py
+++ Parsing Dictionary into key list and value list.py
@@ -1,11 +1,10 @@
 student ={ "Name": "Erika", "ID": "201620A", "Department": "EC", "Program": "Undergraduate"}
 Keylist = []
-for x in student:
-   Keylist.append(x)
-print("Key List = ",Keylist)
-
 Valuelist = []
 for x in student:
-      Valuelist.append(student[x])
+   Keylist.append(x)
+   Valuelist.append(student[x])
+
+print("Key List = ",Keylist)
 print("Value List = ",Valuelist)
     
